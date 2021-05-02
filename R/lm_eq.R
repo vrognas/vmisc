53f08951e0d04ec6332b59a9f27153ec664c5b44
Viktor Rognås <viktor.rognas@farmaci.uu.se>
2021-05-02 18:44:29 +0200

    Add lm_eq

---
+++ R/lm_eq.R
@@ -0,0 +1,21 @@
+#' lm_eq function
+#'
+#' This function allows you to output the equation as well as the adjusted
+#' R-squared, for a linear model in a ggplot.
+#' @keywords lm linear model equation
+#' @export
+#' @examples
+#' cars %>%
+#' ggplot(aes(x = x, y = y)) +
+#' geom_smooth(method = "lm") +
+#' lm_eq()
+
+lm_eq <- function() {
+  ggpmisc::stat_poly_eq(
+    ggplot2::aes(
+      label =  paste(stat(eq.label), stat(adj.rr.label), sep = "*\", \"*")
+    ),
+    formula = y ~ x,
+    parse = TRUE
+  )
+}
